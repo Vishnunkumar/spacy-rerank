7744b3d84650611341a785df1736334f3b7eea29
Vishnu Nandakumar <38393302+Vishnunkumar@users.noreply.github.com>
2024-05-17 12:12:48 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requirements = [
 
 setuptools.setup(
     name="spacyrerank",
-    version="0.0.5",
+    version="0.0.6",
     author="Vishnu Nandakumar",
     author_email="nkumarvishnu25@gmail.com",
     description="Rank phrases and text based on query by leveraging hugging-face models.",
